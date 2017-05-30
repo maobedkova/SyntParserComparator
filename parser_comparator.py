dd4889ad4a7ba395d037528f0da424007b9a3208
Maria Obedkova <maryobedkova@gmail.com>
2017-05-31 02:03:18 +0300

    make it universal

--- parser_comparator.py
+++ parser_comparator.py
@@ -4,28 +4,27 @@ __author__ = "maobedkova"
 
 import numpy
 
-path = 'C:/Users/Maria/OneDrive/HSE/Projects/Sketches/corpora/'
+# path = 'C:/Users/Maria/OneDrive/HSE/Projects/Sketches/corpora/'
 
-conll_text = 'UD-all.conll'
-txt_text = 'raw_text.txt'
+# conll_text = 'UD-all.conll'
+# txt_text = 'raw_text.txt'
+#
+# golden_standard_file = 'UD-all.conll'
+# udpipe_file = 'parsed_udpipe.conll'
+# rusyntax_file = 'parsed_rusyntax.conll'
+# syntaxnet_file = 'changed_syntaxnet.conll'
 
-golden_standard_file = 'UD-all.conll'
-udpipe_file = 'parsed_udpipe.conll'
-rusyntax_file = 'parsed_rusyntax.conll'
-syntaxnet_file = 'changed_syntaxnet.conll'
-# syntaxnet_file = 'parsed_syntaxnet.conll'
 
-
-def form_dataset(input_file, output_file, path=path):
+def form_dataset(input_file, output_file):
     """The function for writing a raw text from conll dataset"""
     print ('=== Text formation ===')
-    with open(path + input_file, 'r', encoding='utf-8') as f:
+    with open(input_file, 'r', encoding='utf-8') as f:
         punct_mark = 0
         for line in f:
             if len(line) == 1:
                 continue
             splitted = line.strip().split('\t')
-            with open(path + output_file, 'a', encoding='utf-8') as w:
+            with open(output_file, 'a', encoding='utf-8') as w:
                 # Every sentence from a new line
                 if splitted[0] == '1':
                     if splitted[1] in '"(':
@@ -52,10 +51,10 @@ def form_dataset(input_file, output_file, path=path):
                             w.write(' ' + splitted[1])
 
 
-def syntaxnet_debugger(input_file, output_file, path=path):
+def syntaxnet_debugger(input_file, output_file):
     """The function for debugging improper splitting for SyntaxNet"""
-    f = open(path + input_file, 'r', encoding='utf-8')
-    with open(path + output_file, 'w', encoding='utf-8') as w:
+    f = open(input_file, 'r', encoding='utf-8')
+    with open(output_file, 'w', encoding='utf-8') as w:
         mem_line = ''
         mark = 0
         for line in f:
@@ -79,7 +78,7 @@ def syntaxnet_debugger(input_file, output_file, path=path):
                         w.write(line)
 
 
-def compare_parsers(golden_standard_file, udpipe_file, syntaxnet_file, rusyntax_file):
+def compare_parsers(golden_standard_file, udpipe_file)#, syntaxnet_file, rusyntax_file):
     '''The function for comparing different syntactic parsers'''
 
     def count_accuracy(gs_arr, sp_arr, true, false, accuracy, accuracy_rel):
@@ -165,28 +164,28 @@ def compare_parsers(golden_standard_file, udpipe_file, syntaxnet_file, rusyntax_
     ud_accuracy_rel = []
 
     # Different SyntaxNet scores
-    sn_true = 0
-    sn_false = 0
-    sn_accuracy = []
-    sn_accuracy_rel = []
+    # sn_true = 0
+    # sn_false = 0
+    # sn_accuracy = []
+    # sn_accuracy_rel = []
 
     # Different RuSyntax scores
-    rs_true = 0
-    rs_false = 0
-    rs_accuracy = []
-    rs_accuracy_rel = []
+    # rs_true = 0
+    # rs_false = 0
+    # rs_accuracy = []
+    # rs_accuracy_rel = []
 
     # Golden standard, UDpipe, SyntaxNet and RuSyntax arrays
-    gs_sn_arr = []
     gs_ud_arr = []
-    gs_rs_arr = []
+    # gs_sn_arr = []
+    # gs_rs_arr = []
     ud_arr = []
-    sn_arr = []
-    rs_arr = []
+    # sn_arr = []
+    # rs_arr = []
 
     # Marking for match
-    rs_mark = 0
-    sn_mark = 0
+    # rs_mark = 0
+    # sn_mark = 0
     ud_mark = 0
 
     # Marking for skipping after match
@@ -196,72 +195,73 @@ def compare_parsers(golden_standard_file, udpipe_file, syntaxnet_file, rusyntax_
     quit_mark = 0
 
     # Opening parser`s files
-    ud_file = open(path + udpipe_file,  'r', encoding='utf-8')
-    sn_file = open(path + syntaxnet_file,  'r', encoding='utf-8')
-    rs_file = open(path + rusyntax_file,  'r', encoding='utf-8')
+    ud_file = open(udpipe_file,  'r', encoding='utf-8')
+    # sn_file = open(syntaxnet_file,  'r', encoding='utf-8')
+    # rs_file = open(rusyntax_file,  'r', encoding='utf-8')
 
     # Writing down the results
     with open('parsers_results.txt', 'w', encoding='utf-8') as w:
         # Opening the golden standard
-        with open(path + golden_standard_file, 'r', encoding='utf-8') as gs_file:
+        with open(golden_standard_file, 'r', encoding='utf-8') as gs_file:
             for gs_line in gs_file:
                 if quit_mark == 1:
                     if len(gs_line) == 1:
                         if ud_mark == 1:
                             flow(ud_file)
-                        if sn_mark == 1:
-                            flow(sn_file)
-                        if rs_mark == 1:
-                            flow(rs_file)
-                        quit_mark = 0
+                        # if sn_mark == 1:
+                        #     flow(sn_file)
+                        # if rs_mark == 1:
+                        #     flow(rs_file)
+                        # quit_mark = 0
                     continue
                 if len(gs_line) == 1:
                     continue
-                w.write ('==NEW GS==' + gs_line)
+                # w.write ('==NEW GS==' + gs_line)
                 # Comparison of the golden standard with UDpipe
-                w.write('UD!')
+                # w.write('UD!')
                 n, ud_mark, gs_ud_arr, ud_arr, ud_true, ud_false, ud_accuracy, ud_accuracy_rel = \
                     iter_file(w, ud_file, n, ud_mark, gs_line, ud_arr, gs_ud_arr,
                               ud_true, ud_false, ud_accuracy, ud_accuracy_rel)
                 # Comparison of the golden standard with SyntaxNet
-                w.write ('SN!')
-                n, sn_mark, gs_sn_arr, sn_arr, sn_true, sn_false, sn_accuracy, sn_accuracy_rel = \
-                    iter_file(w, sn_file, n, sn_mark, gs_line, sn_arr, gs_sn_arr,
-                              sn_true, sn_false, sn_accuracy, sn_accuracy_rel)
-                # Comparison of the golden standard with RuSyntax
-                w.write ('RS!')
-                n, rs_mark, gs_rs_arr, rs_arr, rs_true, rs_false, rs_accuracy, rs_accuracy_rel = \
-                    iter_file(w, rs_file, n, rs_mark, gs_line, rs_arr, gs_rs_arr,
-                              rs_true, rs_false, rs_accuracy, rs_accuracy_rel)
+                # w.write ('SN!')
+                # n, sn_mark, gs_sn_arr, sn_arr, sn_true, sn_false, sn_accuracy, sn_accuracy_rel = \
+                #     iter_file(w, sn_file, n, sn_mark, gs_line, sn_arr, gs_sn_arr,
+                #               sn_true, sn_false, sn_accuracy, sn_accuracy_rel)
+                # # Comparison of the golden standard with RuSyntax
+                # w.write ('RS!')
+                # n, rs_mark, gs_rs_arr, rs_arr, rs_true, rs_false, rs_accuracy, rs_accuracy_rel = \
+                #     iter_file(w, rs_file, n, rs_mark, gs_line, rs_arr, gs_rs_arr,
+                #               rs_true, rs_false, rs_accuracy, rs_accuracy_rel)
                 # If some of parsers have different tokenization examples are not considered
-                if (ud_mark + sn_mark + rs_mark) < 3:
+                # if (ud_mark + sn_mark + rs_mark) < 3:
+                if ud_mark == 0:
                     gs_ud_arr = []
-                    gs_sn_arr = []
-                    gs_rs_arr = []
+                    # gs_sn_arr = []
+                    # gs_rs_arr = []
                     ud_arr = []
-                    sn_arr = []
-                    rs_arr = []
+                    # sn_arr = []
+                    # rs_arr = []
                     quit_mark = 1
-                if len(ud_accuracy) == 1200:
-                    break
+                # if len(ud_accuracy) == 1200:
+                #     break
 
         w.write('\nThe number of sentences processed: ' + str(len(ud_accuracy)) + '\n\n')
 
-        w.write('=== Accuracy for UDpipe ===\n')
+        # w.write('=== Accuracy for UDpipe ===\n')
         w.write('Accuracy for the whole text: ' + str(float(ud_true) / float(ud_true + ud_false)) + '\n')
         w.write('Mean accuracy for every sentence: ' + str(numpy.mean(ud_accuracy)) + '\n')
         w.write('Mean accuracy for every sentence with higher weight for root: ' + str(numpy.mean(ud_accuracy_rel)) + '\n\n')
 
-        w.write('=== Accuracy for SyntaxNet ===\n')
-        w.write('Accuracy for the whole text: ' + str(float(sn_true) / float(sn_true + sn_false)) + '\n')
-        w.write('Mean accuracy for every sentence: ' + str(numpy.mean(sn_accuracy)) + '\n')
-        w.write('Mean accuracy for every sentence with higher weight for root: ' + str(numpy.mean(sn_accuracy_rel)) + '\n\n')
-
-        w.write('=== Accuracy for RuSyntax ===\n')
-        w.write('Accuracy for the whole text: ' + str(float(rs_true) / float(rs_true + rs_false)) + '\n')
-        w.write('Mean accuracy for every sentence: ' + str(numpy.mean(rs_accuracy)) + '\n')
-        w.write('Mean accuracy for every sentence with higher weight for root: ' + str(numpy.mean(rs_accuracy_rel)) + '\n')
+        # w.write('=== Accuracy for SyntaxNet ===\n')
+        # w.write('Accuracy for the whole text: ' + str(float(sn_true) / float(sn_true + sn_false)) + '\n')
+        # w.write('Mean accuracy for every sentence: ' + str(numpy.mean(sn_accuracy)) + '\n')
+        # w.write('Mean accuracy for every sentence with higher weight for root: ' + str(numpy.mean(sn_accuracy_rel)) + '\n\n')
+        #
+        # w.write('=== Accuracy for RuSyntax ===\n')
+        # w.write('Accuracy for the whole text: ' + str(float(rs_true) / float(rs_true + rs_false)) + '\n')
+        # w.write('Mean accuracy for every sentence: ' + str(numpy.mean(rs_accuracy)) + '\n')
+        # w.write('Mean accuracy for every sentence with higher weight for root: ' + str(numpy.mean(rs_accuracy_rel)) + '\n')
 
 
 if __name__ == '__main__':
-    compare_parsers(golden_standard_file, udpipe_file, syntaxnet_file, rusyntax_file)
+    compare_parsers(golden_standard_file, udpipe_file)#, syntaxnet_file, rusyntax_file)
